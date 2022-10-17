6156cc9f1d9ff2bc20a028b20c1e93fedf635f94
mugilmalathi <mugilarasan.malathi@gmail.com>
2022-10-17 15:33:50 +0530

    controller and schema changes

--- src/model/account.model.js
+++ src/model/account.model.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     id : {type:String},
-    name:{type: String},
-    email : {type: String},
-    password : {type: String},
+    name:{type: String, required: true},
+    email : {type: String, required: true},
+    password : {type: String, required: true},
     mobile:{type:Number}
 
 },{
