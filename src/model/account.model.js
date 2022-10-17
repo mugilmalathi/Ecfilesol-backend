e4a5ec8e0c966c698942f10d79cdbcaebffc01d3
mugilmalathi <mugilarasan.malathi@gmail.com>
2022-10-17 16:11:19 +0530

    Schema email update

--- src/model/account.model.js
+++ src/model/account.model.js
@@ -3,7 +3,19 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     id : {type:String},
     // name:{type: String, required: true},
-    email : {type: String, required: true},
+    email : {
+        type: String,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        validate: {
+            validator: function(v) {
+                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+            },
+            message: "Please enter a valid email"
+        },
+        required: [true, "Email required"]
+    },
     // password : {type: String, required: true},
     // mobile:{type:Number}
 
