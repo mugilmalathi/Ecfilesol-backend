7e4d996019f0c5e1456d0196d166a8a883223ba3
mugilmalathi <mugilarasan.malathi@gmail.com>
2022-10-17 17:38:20 +0530

    Validation update

--- src/model/account.model.js
+++ src/model/account.model.js
@@ -1,36 +1,58 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    id : {type:String},
-    // name:{type: String, required: true},
-    email : {
-        type: String,
-        trim: true,
-        lowercase: true,
-        unique: true,
-        validate: {
-            validator: function(v) {
-                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
-            },
-            message: "Please enter a valid email"
-        },
-        required: [true, "Email required"]
+const userSchema = new mongoose.Schema(
+  {
+    id: { type: String },
+    name: {
+      type: String,
+      required: true,
+      validate: [validators.notEmpty, "Name is empty"],
     },
-    // password : {type: String, required: true},
-    mobile:{
-            type: String,
-            trim: true,
-            validate: {
-              validator: function (v) {
-                return /^[0-9]{10}/.test(v);
-              },
-              message: '{VALUE} is not a valid 10 digit number!'
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      validate: {
+        validator: function (v) {
+          return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+        },
+        message: "Please enter a valid email",
+      },
+      required: [true, "Email required"],
+    },
+    password: {
+      type: String,
+      validate: {
+        validator: function (v) {
+          return /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/.test(v);
+        },
+        message: "Please enter a valid password",
+        required: [true, "Password required"],
+    },
+    mobile: {
+      type: String,
+      trim: true,
+      validate: [
+            {
+            validator: function (v) {
+            return /^[0-9]{10}/.test(v);
+            },
+            message: "{VALUE} is not a valid 10 digit number!",
+            },
+            {
+            validator: function (v) {
+            return /^{6}[0-9]{10}/.test(v);
+            },
+            message: "{VALUE} is not a valid 10 digit number!",
             }
-    }
-
-},{
+      ]
+    },
+  },
+  {
     versionKey: false,
-    timestamps: true
-})
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model("accAuth", userSchema)
+module.exports = mongoose.model("accAuth", userSchema);
